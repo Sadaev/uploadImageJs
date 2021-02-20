6139e9df5c43bd198168725fc1055cd599d14f73
Садаев Шынгыс <sadaevsh@gmail.com>
2021-02-20 19:56:17 +0600

    ui complite

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ import {upload} from'./upload'
 upload('#file', {
     multi: true,
     accept: ['.png', '.jpg', '.jpeg', 'gif'],
+    onUpload(files){
+        console.log('Files:', files);
+    }
 })
--- upload.js
+++ upload.js
@@ -7,6 +7,8 @@ function bytesToSize(bytes) {
     return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i]
  }
 
+ function noop () {}
+
  const element = (tag, classes = [], content) => {
     const node = document.createElement(tag)
     if(classes.length){
@@ -15,19 +17,20 @@ function bytesToSize(bytes) {
     if(content){
         node.textContent = content
     }
+    return node
  }
 
 
 export function upload(selector, options = {}){
     let files = []
+    const onUpload = options.onUpload ?? noop
     const input = document.querySelector(selector)
     const preview = element('div', ['preview'])
     const open = element('button', ['btn'], 'Открыть')
     const upload = element('button', ['btn','primary'], 'Загрузить')
+    upload.style.display = 'none'
 
-    if(options.multi){
-        input.setAttribute('multiple', true)
-    }
+    if(options.multi) input.setAttribute('multiple', true)
 
     if(options.accept && Array.isArray(options.accept)){
         input.setAttribute('accept', options.accept.join(','))
@@ -45,6 +48,8 @@ export function upload(selector, options = {}){
         }
         files = Array.from(event.target.files)
         preview.innerHTML = ''
+        upload.style.display = 'inline'
+
         files.forEach(file => {
             if(!file.type.match('image')){
                 return
@@ -73,15 +78,27 @@ export function upload(selector, options = {}){
         }
         const {name} = event.target.dataset
         files = files.filter(file => file.name !== name)
-        
+        if(!files.length) upload.style.display = 'none'
         const block = preview
         .querySelector(`[data-name="${name}"]`).closest('.preview-image')
-
         block.classList.add('removing')
         setTimeout(() => block.remove(), 300);
+    }
+
+    const clearPreview = el => {
+        el.style.bottom = '4px'
+        el.innerHTML = `<div class="preview-info-progress"></div>`
+    }
+
+    const uploadHandler = () => {
+        preview.querySelectorAll('.preview-remove').forEach(e => e.remove())
+        const previewInfo = preview.querySelectorAll('.preview-info')
+        previewInfo.forEach(clearPreview)
+        onUpload(files)
     }
 
     open.addEventListener('click', triggerInput)
     input.addEventListener('change', changeHandler)
     preview.addEventListener('click', removeHandler)
+    upload.addEventListener('click', uploadHandler)
 }
